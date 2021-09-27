05238142db140a8c9902372c4aaf7a91440802a9
askePhoenix <aistarteverything@gmail.com>
2021-09-27 14:32:27 +0900

    login and page #2
    - logout 추가
    - login page 오타 제거

--- src/main/java/com/capstone/kmcapstone/user/controller/UserPageController.java
+++ src/main/java/com/capstone/kmcapstone/user/controller/UserPageController.java
@@ -1,7 +1,12 @@
 package com.capstone.kmcapstone.user.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserPageController {
@@ -18,5 +23,12 @@ public class UserPageController {
     @GetMapping("/sign-on")
     public String signOnPage() {
         return "sign-on";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
